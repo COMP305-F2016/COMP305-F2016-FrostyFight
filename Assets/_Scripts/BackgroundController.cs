3a4ab28cce450062fbbf3261c2c6de7b5a8cb875
Angela <angelayh.liu@gmail.com>
2016-09-28 00:23:34 -0400

    added background

---
+++ Assets/_Scripts/BackgroundController.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BackgroundController : MonoBehaviour {
+    // PRIVATE INSTANCE VARIABLES 
+    [SerializeField]
+    private int _speed;
+    private Transform _transform;
+
+    //PUBLIC PROPERTIES
+
+    public int Speed {
+        get {
+            return this._speed;
+
+        }
+        set {
+            this._speed = value;
+        }
+    }
+	// Use this for initialization
+	void Start () {
+        this._transform = this.GetComponent<Transform>();
+        this._speed = 1;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        this._move();
+        this._checkBounds();
+ 	}
+
+    /*this method moves the game object down the screen by _speed px every frame
+     */
+
+    private void _move() {
+        Vector2 newPosition = this._transform.position;
+        newPosition.x -= this._speed;
+        this._transform.position = newPosition;
+    }
+
+    /*this method checks to see if the game object meet the side-border of the screen
+    */
+    private void _checkBounds() {
+        if (this._transform.position.x <= -90f) {
+            this._reset();
+        }
+    }
+
+    /*this method resets the game object to the original position
+    */
+    private void _reset() {
+        this._transform.position = new Vector2(0f, 0f);
+    }
+}
